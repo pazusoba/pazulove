a43d7c0e21de28deb9876b4545f72671394d0a02
Yiheng <development.henryquan@gmail.com>
2021-02-22 22:27:15 +1100

    generate better boards
    generated boards have more varieties now

--- scripts/csv_board_convert.py
+++ scripts/csv_board_convert.py
@@ -6,12 +6,17 @@ import sys
 
 OUTPUT_TABLE = ["", "R", "B", "G", "L", "D", "H", "J", "P", "", "", "", ""]
 
+def convert_board(board):
+    output = ""
+    for num in board.replace(" ", "").split(","):
+        if num.isnumeric():
+            output += OUTPUT_TABLE[int(num)]
+        else:
+            output = "Input '{}' wasn't valid".format(board)
+            break
+    return output
+
 board = "5, 3, 5, 4, 6, 4, 6, 5, 6, 3, 3, 5, 2, 5, 4, 5, 3, 5, 6, 4, 2, 4, 2, 2, 1, 4, 1, 2, 8, 7"
 if len(sys.argv) > 1:
     board = sys.argv[1] if "," in sys.argv[1] else board
-
-for num in board.replace(" ", "").split(","):
-    if num.isnumeric():
-        print(OUTPUT_TABLE[int(num)], end="")
-    else:
-        exit("Input '{}' wasn't valid".format(board))
+print(convert_board(board))
--- scripts/csv_board_gen.py
+++ scripts/csv_board_gen.py
@@ -2,15 +2,27 @@
 Generate random boards
 """
 
-from random import randint
+from random import choice, randint
+from csv_board_convert import convert_board
 
 # 30 for 6 x 5, 42 for 7 x 6
 board = 30
-count = 30
+count = 700
 
-for i in range(0, count):
-    for j in range(0, count):
-        # from fire orb to unknown
-        orb = randint(1, 6)
-        print("{}{} ".format(orb, "," if j < board - 1 else ""), end='')
-    print()
+with open("../data/data.csv", "a") as csv:
+    for i in range(0, count):
+        new_board = ""
+        ORBS = list(range(1, 7))
+
+        # 50% chance to get a board with less orbs
+        if randint(0, 1) == 0:
+            number_of_orbs = randint(2, 5)
+            while len(ORBS) > number_of_orbs:
+                ORBS.remove(choice(ORBS))
+
+        for j in range(0, board):
+            orb = choice(ORBS)
+            new_board += "{}{}".format(orb, ", " if j < board - 1 else "")
+        # print(convert_board(new_board))
+        csv.write(new_board + "\n")
+    csv.close()

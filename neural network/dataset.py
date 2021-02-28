51fdf37faea12228264058461289945bd46c6812
Yiheng <development.henryquan@gmail.com>
2021-02-28 11:13:23 +1100

    implemented dataset and debugging
    it almost worked but it didn't work

--- neural network/dataset.py
+++ neural network/dataset.py
@@ -1,20 +1,48 @@
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd
+import torch
 
-class TrainDataset(Dataset):
+class BaseDataset(Dataset):
+    def __init__(self, csv_file):
+        self.data = pd.read_csv(csv_file)
+        self.iterator = iter(self[x] for x in range(len(self)))
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        curr = self.data.iloc[index].values
+        # convert to tensor and float type
+        return (torch.tensor(curr[:-1], dtype=float), torch.tensor(curr[-1], dtype=float))
+
+    def __iter__(self):
+        return self.iterator
+
+class TrainDataset(BaseDataset):
     """
     loaded from data_random.csv
     """
-    pass
+    def __init__(self):
+        super().__init__("data/data_random.csv")
 
-class TestDataset(Dataset):
+class TestDataset(BaseDataset):
     """
     loaded from data_test.csv
     """
-    pass
+    def __init__(self):
+        super().__init__("data/data_test.csv")
 
-class SmallDataSet(Dataset):
+class SmallDataSet(BaseDataset):
     """
     loaded from data_small.csv
     """
-    pass
+    def __init__(self):
+         super().__init__("data/data_small.csv")
+
+# test
+# train = SmallDataSet()
+# (first, second) = train[0]
+# print(first, second)
+# print(len(train))
+# for i in train:
+#     print(i)
--- neural network/test.py
+++ neural network/test.py
@@ -1,27 +1,21 @@
 from pazulove import PazuLove
 from torch.utils.data import DataLoader
+from dataset import TestDataset
 import torch
-import pandas as pd
 
 model = PazuLove(32, 16, 16, 1)
 model.load_state_dict(torch.load("model.ckpt"))
 model.eval()
 
-# csv_data = pd.read_csv("data/data_random.csv")
-csv_data = pd.read_csv("data/data_test.csv")
-
-test_loader = DataLoader(csv_data.iloc[:, :-1].values)
-test_output = torch.tensor(csv_data.iloc[:, -1].values, dtype=torch.float)
+test_loader = DataLoader(TestDataset)
 
 # test the model
 with torch.no_grad():
     correct = 0
     total = 0
 
-    for i, data in enumerate(test_loader):
-        output = test_output[i]
-
-        prediction = model(data.float())
+    for (data, output) in test_loader:
+        prediction = model(data)
         predicted, actual = prediction[0][0].item(), output.item()
         predicted_combo, actual_combo = int(predicted / 1000), int(actual / 1000)
 

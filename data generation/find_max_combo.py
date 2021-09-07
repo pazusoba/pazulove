d26f9abe18d00f433b77a3523e1a8223e62fc728
Yiheng <development.henryquan@gmail.com>
2021-09-07 18:27:02 +1000

    call findMaxCombo from so

--- data generation/find_max_combo.py
+++ data generation/find_max_combo.py
@@ -1,1 +1,24 @@
-libpazulove.so
+"""
+Call methods exported in the shared library
+"""
+from ctypes import *
+from typing import List
+import os
+
+LIBRARY = "libpazulove.so"
+if not os.path.exists(LIBRARY):
+    print("{} is not found at current directory".format(LIBRARY))
+
+libpazusoba = CDLL(LIBRARY, winmode=0)
+libpazusoba.findMaxCombo.restype = c_int
+libpazusoba.findMaxCombo.argtypes = (c_char_p, c_uint)
+
+
+def findMaxCombo(board: str, step: int) -> int:
+    # convert to c string
+    c_board = c_char_p(board.encode('ascii'))
+    return libpazusoba.findMaxCombo(c_board, step)
+
+if __name__ == '__main__':
+    combo = findMaxCombo("RHLBDGPRHDRJPJRHHJGRDRHLGLPHBB", 10)
+    print("Max combo is {}\n".format(combo))
